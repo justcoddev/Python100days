1e7e3ecb387dc39b2fbbc1231dd987d360fdff08
Teddy <edisonpaulcrz@gmail.com>
2021-10-07 11:57:29 -0500

    Day 007: loop for break continue List

---
+++ day007/042Listas.py
@@ -0,0 +1,10 @@
+#Definir una Lista de tipo str
+nombres = ["Juan","Karla","Ricardo", "María"]
+#imprimir la lista de nombre
+print(nombres)
+#accerder a lso elementos de una lista
+print(nombres[0])
+print(nombres[1])
+#acceder a los elementos de manera inversa
+print(nombres[-1])
+print(nombres[-2])
