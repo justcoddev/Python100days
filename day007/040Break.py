de5102bfb81b7cd6c698b9315529c14856a7acbf
Teddy <edisonpaulcrz@gmail.com>
2021-10-07 11:17:12 -0500

    Day 007: loop for break

---
+++ day007/040Break.py
@@ -0,0 +1,6 @@
+for letra in "Holanda":
+    if letra == "a":
+        print(f"letra encontrada: {letra}")
+        break #romper el ciclo una vez que ha encontrado algo
+else:
+    print("Fin ciclo for")
