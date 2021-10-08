3e0eb7886c74ca94c66a6a7c136963d466a16a96
Teddy <edisonpaulcrz@gmail.com>
2021-10-08 10:11:55 -0500

    Day 008: Tuplas

---
+++ day008/045Tuplas.py
@@ -0,0 +1,11 @@
+#Definit una tupla
+frutas = ('Naranja', 'Platano', 'Guayaba') #siemrpe poner coma al final aunque sea solo un elemento
+print(frutas)
+#saber el largo
+print(len(frutas))
+#acceder a un elemento
+print(frutas[0])
+#navegacion inversa
+print(frutas[-1])
+#acceder a un rango
+print(frutas[0:1]) #sin incluir el último índice
