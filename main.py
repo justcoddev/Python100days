1e02cdf889282e28b5766b7ea10eb1fd7f559efd
Teddy <edisonpaulcrz@gmail.com>
2021-10-08 10:18:42 -0500

    day1

--- main.py
+++
@@ -1,5 +0,0 @@
-#Función input
-numero1 = int(input("Escribe el primer número: "))
-numero2 = int(input("Escribe el segundo número: "))
-resultado = numero1 + numero2
-print("El resultado de la suma es: ", resultado)
