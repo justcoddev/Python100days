1069a558a01c6e2945a52b85354837cc2c59ea59
Teddy <edisonpaulcrz@gmail.com>
2021-10-11 13:59:14 -0500

    Day 010: Funcion return

---
+++ day010/053FuncionReturn.py
@@ -0,0 +1,8 @@
+def sumar(a, b):
+    return a + b
+
+resultado = sumar(4, 5)
+
+print(f' Resultado sumar: {resultado}')
+#tambien se puede llamar directamente la función
+print(f' Resultado sumar: {sumar(4 , 6)}')
