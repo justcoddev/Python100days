aa3925534a24460b30931169142893f8550018f7
Teddy <edisonpaulcrz@gmail.com>
2021-10-11 14:17:08 -0500

    Day 010: Default PArametros de una funcion

---
+++ day010/053DefaultParametrosdeUnaFuncion.py
@@ -0,0 +1,9 @@
+def sumar(a = 0, b = 0):
+    return a + b
+
+resultado = sumar()
+
+print(f' Resultado sumar: {resultado}')
+
+#sobreescribe los valores por default
+print(f' Resultado sumar: {sumar(4 , 6)}')
