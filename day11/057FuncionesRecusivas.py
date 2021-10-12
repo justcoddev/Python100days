5fc462add55399cb54ec06e72d65ede2ae864bb2
Teddy <edisonpaulcrz@gmail.com>
2021-10-11 23:11:34 -0500

    Day 011: Funciones Recursivas

---
+++ day11/057FuncionesRecusivas.py
@@ -0,0 +1,7 @@
+def factorial(numero):
+    if numero == 1:
+        return 1
+    else:
+        return numero * factorial(numero-1)
+resultado = factorial(5)
+print(f'El factorial de 5 es: {resultado}')
