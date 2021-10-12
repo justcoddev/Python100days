84a9d0a2ca27b3bf903d021c860dc8eaeb35266d
Teddy <edisonpaulcrz@gmail.com>
2021-10-12 13:46:53 -0500

    Day 012: Ejercicio_001a.py

---
+++ day003/Ejercicio_001a.py
@@ -0,0 +1,12 @@
+# 1)	Existe una unidad de peso llamada libra. Una libra equivale aproximadamente a 0,454 kg.
+# a) Hacer un programa para ingresar por teclado el peso de un persona en kgs y que calcule y
+# emita por pantalla el equivalente en libras.
+# Atención: El programa solamente debe solicitar un solo dato: la cantidad de kgs a convertir.
+# b) Hacer un programa para ingresar por teclado el peso de un persona en libras y que calcule y
+# emita por pantalla el equivalente en kgs.
+# Atención: El programa solamente debe solicitar un solo dato: la cantidad de libras a convertir.
+#a
+print('Calcular Unidad de Peso')
+kilog = int(input('Ingrese peso en Kilogramos: '))
+libras = kilog * 0.454
+print(f'Su equivalente a libras es {libras}')
