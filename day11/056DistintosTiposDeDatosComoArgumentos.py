4f5b2cd6ee66082b83601ff314aeb41878bda9c9
Teddy <edisonpaulcrz@gmail.com>
2021-10-11 20:52:15 -0500

    Day 011: Distintos Tipos de Datos como Argumentos

---
+++ day11/056DistintosTiposDeDatosComoArgumentos.py
@@ -0,0 +1,9 @@
+def desplegarNombres(nombres):
+    for nombre in nombres:
+        print(nombre)
+
+nombres =['Juan', 'Karla', 'Guillermo']
+desplegarNombres(nombres)
+desplegarNombres('Carlos')
+desplegarNombres((10, 11))
+desplegarNombres([10, 11])
