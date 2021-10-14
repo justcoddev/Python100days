13c699a306fe4991c42aa46f8d6c39dfc26e451e
Teddy <edisonpaulcrz@gmail.com>
2021-10-13 21:25:08 -0500

    Day 013: Ejercicio_008.py

---
+++ day003/Ejercicio_008.py
@@ -0,0 +1,12 @@
+# 8)Un negocio de venta de alfajores le pide a Ud., futuro programador, que le desarrolle un programa
+# teniendo en cuenta las siguientes condiciones:
+# La caja de 12 alfajores se vende a $5.- y cada alfajor suelto a $ 0,50.
+# El programa debe solicitar al usuario cuantos alfajores compró un cliente y
+# luego calcular el importe a pagar por el mismo.
+# Tener en cuenta que por cada 12 alfajores se debe calcular una caja, y el excedente se calcula como suelto.
+# Por ejemplo: Si la cantidad ingresada son 15 alfajores, el programa
+# calculará: 1 caja y 3 sueltos, es decir $5.- + $0,50 x 3 = $ 6,50.
+# Si cantidad ingresada son 28 alfajores, el programa calculará: 2 cajas y 4 sueltos, es decir
+# $10.- + $0,50 x 4 = $ 12.-.
+# Si cantidad ingresada son 8 alfajores, el programa calculará: 0 cajas y 8 sueltos, es decir
+# $0,50 x 8 = $ 4.-.
