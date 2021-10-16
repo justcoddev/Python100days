cf9559f431f31d9aa150ae63058056c7ada5338e
Teddy <edisonpaulcrz@gmail.com>
2021-10-16 14:43:24 -0500

    Día 016: Trabajo Práctico (Decisión) 3

---
+++ day005/Ejercicio_016.py
@@ -0,0 +1,14 @@
+# 8)Hacer un programa para ingresar por teclado las cuatro notas de los exámenes parciales obtenidas
+# por un alumno en una determinada materia y luego emitir el cartel aclaratorio que corresponda,
+# de acuerdo a las siguientes condiciones:
+# - “Promociona”, si obtuvo en los cuatro exámenes nota 7 o más.
+# - “Rinde examen final”, si obtuvo nota 4 o más en por lo menos tres exámenes.
+# - “Recupera Parciales”, si obtuvo nota 4 o más en por lo menos uno de los exámenes.
+# - “Recursa la materia”, si no aprobó ningún examen parcial.
+#
+# El programa de emitir UNO SOLO de los carteles anteriores.
+print('#####')
+num_1 = int(input('Ingrese primera nota: '))
+num_2 = int(input('Ingrese segunda nota: '))
+num_3 = int(input('Ingrese tercera nota: '))
+num_4 = int(input('Ingrese cuarta nota: '))
