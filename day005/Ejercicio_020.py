98b1b026bfcad5f7dbe8d635381f4049e1ed6fa2
Teddy <edisonpaulcrz@gmail.com>
2021-10-16 17:58:47 -0500

    Día 016: Trabajo Práctico (Decisión) 3

---
+++ day005/Ejercicio_020.py
@@ -0,0 +1,14 @@
+# 12)	Una empresa de electricidad cobra el servicio a sus clientes de acuerdo a la siguiente escala:
+#
+# $ 0,10 por kilovatio por los primeros 100 kilovatios de consumo.
+# $ 0,12 por kilovatio por el consumo de 101 a 200 kilovatios.
+# $ 0,15 por kilovatio por el consumo de 201 kilovatios en adelante.
+
+# Hacer un programa para que dado el consumo en kilovatios de un determinado cliente,
+# el programa calcule e informe el total a pagar por el mismo.
+# Ejemplo 1: Si se ingresa un consumo de 55 kilovatios, entonces el programa calculará:
+# $ 0,10 x 55= $ 5,50
+# Ejemplo 2: Si se ingresa un consumo de 125 kilovatios, entonces el programa calculará:
+# $ 0,10 x 100 + $ 0,12 x 25=$ 13
+# Ejemplo 3: Si se ingresa un consumo de 250 kilovatios, entonces el programa calculará:
+# $ 0,10 x 100 + $ 0,12 x 100 + $ 0,15 x 50 = $ 29,50.
