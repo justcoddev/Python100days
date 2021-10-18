de5de45b7d7fc6c92a01b00f268b834d39784b01
Teddy <edisonpaulcrz@gmail.com>
2021-10-17 23:53:19 -0500

    Día 017: Trabajo Práctico (Decisión) 4

--- day005/Ejercicio_020.py
+++ day005/Ejercicio_020.py
@@ -1,9 +1,7 @@
 # 12)	Una empresa de electricidad cobra el servicio a sus clientes de acuerdo a la siguiente escala:
-#
 # $ 0,10 por kilovatio por los primeros 100 kilovatios de consumo.
 # $ 0,12 por kilovatio por el consumo de 101 a 200 kilovatios.
 # $ 0,15 por kilovatio por el consumo de 201 kilovatios en adelante.
-
 # Hacer un programa para que dado el consumo en kilovatios de un determinado cliente,
 # el programa calcule e informe el total a pagar por el mismo.
 # Ejemplo 1: Si se ingresa un consumo de 55 kilovatios, entonces el programa calculará:
@@ -15,3 +13,22 @@
 print('#####')
 name = input('Nombre del cliente: ')
 kilovatios = int(input('Ingrese Kilovatios consumidos: '))
+if kilovatios <= 100:
+    pkilovatios = kilovatios * 0.10
+    print(f'Su consumo ha sido calculado:\n$0.10 * {kilovatios} kilovatios = {pkilovatios} kilovatios')
+elif 101 < kilovatios < 200:
+    # skilovatiosdiv = kilovatios / 100
+    # pkilovatios = int(skilovatiosdiv * 0.10)
+    pkilovatios = 0.10 * 100
+    skilovatiosmod = kilovatios % 100
+    skilovatios = skilovatiosmod * 0.12
+    total = pkilovatios + skilovatios
+    print(f'Su consumo ha sido calculado:\n$0.10 * 100 kilovatios + $0.12 * {skilovatiosmod} = {total} kilovatios')
+elif kilovatios > 201:
+    # 200 kilovatios = 22
+    pkilovatios = 0.10 * 100
+    skilovatios2 = 0.12 * 100
+    skilovatiosmod = kilovatios % 200
+    skilovatios = skilovatiosmod * 0.15
+    total = pkilovatios + skilovatios2 + skilovatios
+    print(f'Su consumo ha sido calculado:\n$0.10 * 100 kilovatios + $0.12 * 100 + $0.15 * {skilovatiosmod} = {total} kilovatios')
