b65ef0ae2f360945663cacd8e1f51cdd83f756c7
Teddy <edisonpaulcrz@gmail.com>
2021-10-17 22:30:09 -0500

    Día 017: Trabajo Práctico (Decisión) 4

---
+++ day005/Ejercicio_021.py
@@ -0,0 +1,19 @@
+# 13)	Una empresa de venta de boletos de micros tiene distintas tarifas según el destino, servicio (común o
+# diferencial) y compañía elegida por el pasajero. La siguiente tabla indica los precios a pagar por el servicio
+# común por pasajero:
+# Compañía	Destino 1	Destino 2	Destino 3
+#    1	    $ 20.-  	$ 15.-	    $ 30.-
+#   2	    $ 22.-	    $ 16,50	    $ 33.-
+#   3	    $ 24.-  	$ 18.-  	$ 36.-
+# El servicio diferencial cuesta un 20% más.
+# Además, si el pasajero compra 5 o más pasajes juntos se ofrece un descuento del 15% .
+# El dueño de la empresa le solicitó a Ud., futuro programador,
+# un programa para ingresar los siguientes datos por cada venta:
+#
+# -	Número de Destino (1 a 3)
+# -	Compañía (1, 2, 3)
+# -	Cantidad de pasajes solicitados por el pasajero
+# -	Servicio (1=  común, 2= diferencial )
+#
+# El programa sólo permite ingresar una venta por vez y calcula y emite el importe neto a pagar.
+#
