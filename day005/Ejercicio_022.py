5eec046edcd24c895a517134a1de92cbfa19beb3
Teddy <edisonpaulcrz@gmail.com>
2021-10-17 22:33:23 -0500

    Día 017: Trabajo Práctico (Decisión) 4

---
+++ day005/Ejercicio_022.py
@@ -0,0 +1,14 @@
+# 14)	Una fórmula permite determinar si una persona tiene un peso normal, bajo o excedido.
+# La fórmula es la siguiente:
+#
+# X=Estatura (en cm.)-Peso (en Kg.)
+# 	       100
+# El valor de X es un coeficiente que se relaciona con la edad de la persona de la siguiente manera:
+#                    Hasta 20 años	       Más de 20 hasta 30  	    Más de 30 hasta 40	        Más de 40
+# Peso normal	    X entre 0,90 y 1,10	    X entre 0,85 y 1,15 	X entre 0,80 y 1,10	        X entre 0,75 y 1,10
+# Bajo peso     	X más de 1,10	            X más de 1,15	    X más de 1,10	          X más de 1,10
+# Excedido	        X menos de 0,90             X menos de 0,85 	X menos de 0,80	            X menos de 0,75
+# Confeccionar un programa para que a partir del ingreso de los datos: estatura (en cm),
+# peso (en Kg) y edad (en años), informe si la persona tiene un peso normal, bajo o excedido.
+# Por ejemplo si se ingresa Estatura: 180 cm, Peso: 70 kg, Edad: 28, se calculará:
+# 180 – 70=110 / 100 = 1,1. Como la edad está entre 20 y 30, X = 1,1 es Peso Normal.
