e6b4b117f341b7c8d215988597dc63a74a688664
Teddy <edisonpaulcrz@gmail.com>
2021-10-20 23:13:28 -0500

    Día 020: Trabajo Práctico (Ciclos)

---
+++ day007/Ejercicio_023.py
@@ -0,0 +1,4 @@
+# 1)	Hacer un programa para ingresar la edad y la altura de 50 personas. Luego se pide determinar e informar:
+# 	a) La edad promedio de las 50 personas.
+# 	b) La edad promedio de las personas con altura entre 1,60 mts. y 1,80 mts. inclusive.
+# 	c) Cantidad de personas con altura mayor a 1,70 mts.
