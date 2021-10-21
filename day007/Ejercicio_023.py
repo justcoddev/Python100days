3f7c2ac4a061716e074d7eb37f7b3e6471cea541
Teddy <edisonpaulcrz@gmail.com>
2021-10-21 18:59:25 -0500

    Día 021: Trabajo Práctico (Ciclos) 2

--- day007/Ejercicio_023.py
+++ day007/Ejercicio_023.py
@@ -3,9 +3,17 @@
 # 	b) La edad promedio de las personas con altura entre 1,60 mts. y 1,80 mts. inclusive.
 # 	c) Cantidad de personas con altura mayor a 1,70 mts.
 print('####')
-# altura = int(input('Escriba la altura: '))
 acumulador = 0
+acumulador2 = 0
+contador = 0
 for i in range(4):
     edad = int(input('Escriba la edad: '))
+    altura = int(input('Escriba la altura: '))
     acumulador += edad
-print(f'promedio: {acumulador}')
+    if 1.60 <= altura <= 1.80:
+        acumulador2 += edad
+        if altura > 170:
+            contador += 1
+print(f'La edad promedio de las 50 personas es: {acumulador}')
+print(f'La edad promedio de las personas con altura entre 1,60 mts. y 1,80 mts es: {acumulador2}')
+print(f'La cantidad de personas con altura mayor a 1,70 mts es: ')
