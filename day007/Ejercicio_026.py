63bf87c97b98e1a850989d35f57dc1ed0b8b1116
Teddy <edisonpaulcrz@gmail.com>
2021-10-20 23:21:21 -0500

    Día 020: Trabajo Práctico (Ciclos)

---
+++ day007/Ejercicio_026.py
@@ -0,0 +1,3 @@
+# 4)Hacer un programa para que dado un número igual o superior a 2 determine si es perfecto o no.
+# 	Un número es perfecto cuando es igual a la suma de sus divisores positivos menores que él.
+# 	Por ejemplo el 6, que es igual a la suma de 1+2+3.
