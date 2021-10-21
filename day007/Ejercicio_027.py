92555c9f6129dfc2c0e6fd7b7e6d62fca5204ddd
Teddy <edisonpaulcrz@gmail.com>
2021-10-20 23:27:22 -0500

    Día 020: Trabajo Práctico (Ciclos)

---
+++ day007/Ejercicio_027.py
@@ -0,0 +1,9 @@
+# 5)	Hacer un programa para ingresar por teclado el nombre, sueldo y la antigüedad de los 30 empleados de una
+# # empresa. Cada registro está compuesto por los siguientes datos:
+# - Nombre del empleado (30 caracteres)
+# - Sueldo
+# - Antigüedad
+# Se pide determinar e informar:
+#
+# a) Cual es el nombre del empleado con mayor sueldo y cual es su antigüedad.
+# b) Informar la antigüedad promedio de los empleados con sueldo mayor a $500.-
