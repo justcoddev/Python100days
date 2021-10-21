4a37f86b14d67a1b49add90371601acfec9cc338
Teddy <edisonpaulcrz@gmail.com>
2021-10-20 23:18:18 -0500

    Día 020: Trabajo Práctico (Ciclos)

---
+++ day007/Ejercicio_025.py
@@ -0,0 +1,5 @@
+# 3)Hacer un programa para ingresar las 20 notas de los alumnos de un curso, luego informar:
+#   a) La cantidad de alumnos aprobados (nota >= 4)
+# 	b) La cantidad de alumnos aplazados (nota <= 3)
+# 	c) La cantidad de alumnos promocionados (nota >=9)
+# Atención: Los alumnos promocionados deben ser contabilizados también como aprobados.
