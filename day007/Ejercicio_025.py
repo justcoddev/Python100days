2cc92df51f2d4204c1bb3c1e8e555a55b39fb29c
Teddy <edisonpaulcrz@gmail.com>
2021-10-22 16:13:02 -0500

    Día 022: Trabajo Práctico (Ciclos) 3

--- day007/Ejercicio_025.py
+++ day007/Ejercicio_025.py
@@ -3,3 +3,19 @@
 # 	b) La cantidad de alumnos aplazados (nota <= 3)
 # 	c) La cantidad de alumnos promocionados (nota >=9)
 # Atención: Los alumnos promocionados deben ser contabilizados también como aprobados.
+
+print('###')
+apr = 0
+apl = 0
+pro = 0
+for i in range(5):
+    nota = int(input('Ingrese nota '+str(i)+':'))
+    if nota >= 4:
+        apr += 1
+    elif nota <= 3:
+        apl += 1
+    elif nota >= 9:
+        pro += 1
+print(f'Aprobados: {apr}')
+print(f'Aplazados: {apl}')
+print(f'Promocionados: {pro}')
