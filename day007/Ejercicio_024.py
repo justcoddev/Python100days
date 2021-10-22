14bf2b63a451e7bdfe821538e5c371aeb4eded51
Teddy <edisonpaulcrz@gmail.com>
2021-10-21 19:56:31 -0500

    Día 021: Trabajo Práctico (Ciclos) 2

--- day007/Ejercicio_024.py
+++ day007/Ejercicio_024.py
@@ -1,6 +1,6 @@
 # 2)Hacer un programa para ingresar por teclado una lista de 10 números, luego determinar e  informar cuantos son
 # positivos, cuantos son negativos, y cuantos iguales a cero.
-print('####')
+print('Ingrese 10 números')
 numeros = []
 positivo = 0
 negativo = 0
