a90caa3c424022148322c45863342eafb7d209d6
Teddy <edisonpaulcrz@gmail.com>
2021-10-21 19:48:28 -0500

    Día 021: Trabajo Práctico (Ciclos) 2

--- day007/Ejercicio_024.py
+++ day007/Ejercicio_024.py
@@ -1,2 +1,8 @@
 # 2)Hacer un programa para ingresar por teclado una lista de 10 números, luego determinar e  informar cuantos son
 # positivos, cuantos son negativos, y cuantos iguales a cero.
+print('####')
+numeros = []
+for i in range(10):
+    lista = input('Ingrese número '+str(i+1)+': ')
+    numeros.append(lista)
+print(numeros)
