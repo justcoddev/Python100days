14604469864dfd3911d3b2e629da62f726d5cd11
Teddy <edisonpaulcrz@gmail.com>
2021-10-22 21:18:57 -0500

    Día 022: Trabajo Práctico (Ciclos) 3

--- day007/Ejercicio_026.py
+++ day007/Ejercicio_026.py
@@ -1,3 +1,11 @@
 # 4)Hacer un programa para que dado un número igual o superior a 2 determine si es perfecto o no.
 # 	Un número es perfecto cuando es igual a la suma de sus divisores positivos menores que él.
 # 	Por ejemplo el 6, que es igual a la suma de 1+2+3.
+
+print('###')
+# acumulador = 0
+num = int(input('Ingrese número: '))
+contador = 0
+for perfecto in range(num):
+    if (perfecto % 2) == 0:
+        contador = contador + num / 2
