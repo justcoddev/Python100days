7b50956e625288c1b92dfbc45813d6d4ef243410
Teddy <edisonpaulcrz@gmail.com>
2021-10-24 20:43:28 -0500

    Día 023: Trabajo Práctico (Ciclos) 4

---
+++ day007/Ejercicio_029.py
@@ -0,0 +1,14 @@
+# 7)	Se ingresan los datos con la producción de los operarios de una empresa en el último mes.
+# Cada registro contiene:
+# -	Número de operario (1 a 20)
+# -	Nombre del operario (30 caracteres)
+# -	Sector donde trabaja (1, 2, 3)
+# -	Total de piezas defectuosas
+# -	Total de piezas no defectuosas
+# Existe un total de 20 registros (uno para cada operario). Cada operario trabaja en alguno de los
+# tres sectores 1, 2 ó 3.
+# Se pide determinar e informar:
+# a) Para cada uno de los 3 sectores el nombre y número del operario que obtuvo mayor cantidad de piezas defectuosas.
+# (se emitirían 6 resultados: 2 nombres y números de operario para cada uno de los 3 sectores).
+# b) El sector cuyos empleados totalizaron mayor porcentaje de piezas defectuosas respecto al total de piezas
+# fabricadas en ese sector.
