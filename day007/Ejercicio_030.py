7786b3ee41339630177a6b82134abd02d8776bd7
Teddy <edisonpaulcrz@gmail.com>
2021-10-24 20:44:45 -0500

    Día 023: Trabajo Práctico (Ciclos) 4

---
+++ day007/Ejercicio_030.py
@@ -0,0 +1,13 @@
+# 8)	Una empresa fabrica dos productos A y B y desea saber cual de ellos es el más aceptado en el mercado. Para
+# ello realiza una encuesta y por cada persona interrogada se obtiene un par de valores, el primer valor del par
+# indica la aceptación o no del producto A según sea 1 ó 0, el segundo valor del par corresponde al producto B. Por
+# ejemplo el par 1,0 significa que el encuestado acepta el producto A pero no el B. Se pide hacer un programa para
+# ingresar el total de encuestados, y luego cada una de las encuestas, y determinar e informar:
+# a)	Cantidad de consumidores que aceptan el producto A. (su opinión del B es indiferente)
+# b)	Cantidad de consumidores que aceptan el producto B. (su opinión del A es indiferente)
+# c)	Cantidad de consumidores que aceptan los dos productos a la vez.
+# d)	Cantidad de consumidores que aceptan el producto A, pero rechazan el B.
+# e)	Cantidad de consumidores que aceptan el producto B, pero rechazan el A.
+# f)	Cantidad de consumidores que rechazan ambos productos a la vez.
+# g)	El porcentaje de aceptación que tiene el producto A sobre el total de encuestados.
+# h)	El porcentaje de aceptación que tiene el producto B sobre el total de encuestados.
