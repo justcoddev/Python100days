c0d07efc8c506df4ee38ee60afd1cf7fb15f6080
Teddy <edisonpaulcrz@gmail.com>
2021-10-24 20:47:18 -0500

    Día 023: Trabajo Práctico (Ciclos) 4

---
+++ day007/Ejercicio_031.py
@@ -0,0 +1,10 @@
+# 9)	Una empresa de bebidas registró por cada venta efectuada la siguiente información:
+#
+# 	- Número de Factura
+# 	- Número de Artículo
+# 	- Cantidad (en litros)
+# 	- Precio Unitario (por litro)
+# El ingreso de datos finaliza con un número de factura igual a cero. Se pide informar:
+# a) Total facturado.
+# b) Cuántos litros del artículo 1 se vendieron en total
+# c) Cuántas facturas de más de $300.- se emitieron
