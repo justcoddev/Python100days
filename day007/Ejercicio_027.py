e10e53df993a294cec6009c26c7b21059e2619dc
Teddy <edisonpaulcrz@gmail.com>
2021-10-25 23:43:10 -0500

    Día 025: Trabajo Práctico (Ciclos) 6

--- day007/Ejercicio_027.py
+++ day007/Ejercicio_027.py
@@ -7,3 +7,5 @@
 #
 # a) Cual es el nombre del empleado con mayor sueldo y cual es su antigüedad.
 # b) Informar la antigüedad promedio de los empleados con sueldo mayor a $500.-
+nombre = input('Ingrse nombre del Empleado: ')
+sueldo = float(input('Ingrese sueldo del empleado: '))
